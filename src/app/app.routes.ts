ee3687b6b6c8aab7271b9041215378ca144c62ca
Ismakolis <esteban.isamel.canencia@gmail.com>
2025-05-21 19:19:35 -0500

    hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MedicoComponent } from './components/medico/medico.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:'',redirectTo:'medico',pathMatch:'full'},
+{path:'medico',component:MedicoComponent}
+
+
+];
